46faa155b5a4cd4605b3563ac2b548f8f04df667
alireza <alireza>
2016-02-28 14:55:21 +0330

    updateText and styling

--- statics/js/app/app.js
+++ statics/js/app/app.js
@@ -1,13 +1,26 @@
 var app = angular.module('playingCard',[]);
 app.controller('cardCtrl',function(){
-    this.text = "alireza";
-    this.title = 'title';
-    this.tag = '';
+    this.card = models.cards[0];
+    this.text = 'text';
+    this.edit = false;
     this.Change = function(){ // when user clicks the try button
         var number = Math.floor(Math.random()*models.cards.length);// creates a random number
-        var card = models.cards[number];// change the text of the view
-        this.text = card.description;
-        this.title = card.title;
-        this.tag = card.tag;
-    }
+        this.card = models.cards[number];// change the card
+        if(this.card.code == 2){
+            var audio = new Audio(this.card.sound);
+            audio.play();
+        }
+        if(this.card.code == 1){
+            var div = $('.thumbnail');
+            div.animate({height: '+=300px', opacity: '0.4'}, "slow");
+            div.animate({width: '+=300px', opacity: '0.8'}, "slow");
+            div.animate({height: '-=300px', opacity: '0.4'}, "slow");
+            div.animate({width: '-=300px', opacity: '0.8'}, "slow",function(){
+                div.css('height','');
+            });
+
+        }
+
+    };
+
 });
